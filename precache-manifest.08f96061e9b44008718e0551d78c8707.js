ad91caced52a6caa8bfe85e46a692daf3fcdffe1
Pratik Var <npc.prat@gmail.com>
2020-10-21 23:29:43 +0530

    Updates

---
+++ precache-manifest.08f96061e9b44008718e0551d78c8707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8180e9a31b3d93632831e09f39e0d313",
+    "url": "/revos/index.html"
+  },
+  {
+    "revision": "dbbcceb71f05f9a48d31",
+    "url": "/revos/static/css/main.1f5c010e.chunk.css"
+  },
+  {
+    "revision": "481ed3edc96da65df3fa",
+    "url": "/revos/static/js/2.2fdeca9b.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/revos/static/js/2.2fdeca9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbbcceb71f05f9a48d31",
+    "url": "/revos/static/js/main.2544ec19.chunk.js"
+  },
+  {
+    "revision": "bf29f62ce3f02ea6f70a",
+    "url": "/revos/static/js/runtime-main.9c0ed1a1.js"
+  }
+]);
